bbbdec4f199ac0c17f81c3d15cb8bb6d3e4c4d32
huseyin <hbayrak.hbayrak@gmail.com>
2021-03-15 22:54:54 +0200

    added R-N Navigation package structure

---
+++ src/Router.js
@@ -0,0 +1,25 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {Introduction ,Jobs, SavedJobs } from './pages';
+
+
+
+const Stack = createStackNavigator();
+
+function Router() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Intro" component={Introduction} />
+        <Stack.Screen name="Jobs" component={Jobs} />
+        <Stack.Screen name="SavedJobs" component={SavedJobs} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Router;
---
+++ src/pages/SavedJobs.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {SafeAreaView, View, Text} from "react-native";
+
+const SavedJobs = props =>{
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>
+                    SavedJobs
+                </Text>
+            </View>
+        </SafeAreaView>
+    )
+};
+
+export {SavedJobs};
